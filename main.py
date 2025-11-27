d4623b32d40ed85ddf7080c30b9deebb5835df36
u-ulim <uiissoop@gmail.com>
2025-11-27 17:38:50 +0900

    251127_start

---
+++ main.py
@@ -0,0 +1,67 @@
+
+# 🚀 print 사용하기  
+
+# print("Hello") 
+
+# print("Hello", " ", "Python")
+
+# print({print("Hello", " ", "Python")})
+
+# print([1, 2, 3, 4, 5])
+
+# print((1, 2, 3, 4, 5))
+
+
+# print(42)
+# print(3.14)
+# print(False)
+
+# print('home', 'user','documents', sep= `/`)
+
+
+
+# print("What is wrong here?)
+
+# print(("Somenting wrong hree")
+
+# ---------------------------------------------------------------
+
+# ✅ Input 명령어 사용  
+
+# name = input("What is your name?")
+# print("Here is my name, " + name) 
+
+# ---------------------------------------------------------------
+
+
+# 🔧 변수 사용하기 
+
+# x, y = 10, 20 
+
+# print(x)
+# print(y)
+
+# ---------------------------------------------------------------
+
+
+# 🧾 primitive data type 
+
+# - Intergers
+#     profile_number = 2030
+#     print(profile_number)
+
+# - Floats
+#     score = float(100.0)
+#     print(score)
+
+# - Boolean(True or False) 
+#     is_active = True 
+
+# - String(str)
+#     teacher_name = str("John Doe")
+#     course_name = str("Python") 
+#     lecture_name = ""
+#     Cisca
+#     Oladipo
+#     """
+#     print(name)
